bbecfba23d792f184816c39035e3d3518f5bbf01
Tom Kearsley <tke29@uclive.ac.nz>
2019-03-12 00:37:33 +1300

    Gridview now identifies which item has been clicked.

--- app/src/main/java/com/example/pianotiles/GridActivity.kt
+++ app/src/main/java/com/example/pianotiles/GridActivity.kt
@@ -2,10 +2,14 @@ package com.example.pianotiles
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.grid.*
 
 class GridActivity: Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
+        val currentScore = 0;
         super.onCreate(savedInstanceState)
         setContentView(R.layout.grid)
 
@@ -14,5 +18,10 @@ class GridActivity: Activity(){
         val adapter = GridViewAdapter(this,tiles)
 
         grid.adapter = adapter
+        grid.setOnItemClickListener{parent, view, position, id ->
+            Toast.makeText(this,"Clicked Item : " + position, Toast.LENGTH_SHORT).show()
+
+
+        }
     }
 }
