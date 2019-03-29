36ac1af9c251304a4b5f9d367ae3250901f6a719
Tom Kearsley <tke29@uclive.ac.nz>
2019-03-29 20:43:00 +1300

    Added game logic. Clicking on black tiles now deletes current row and adds new row at bottom of screen

--- app/src/main/java/com/example/pianotiles/GridActivity.kt
+++ app/src/main/java/com/example/pianotiles/GridActivity.kt
@@ -9,45 +9,66 @@ import kotlinx.android.synthetic.main.grid.*
 import java.util.*
 
 class GridActivity: Activity(){
+    val gameGrid = ArrayList<PianoTile>()
+
+    val tiles = ArrayList<ArrayList<PianoTile>>()
+    val whiteTile = PianoTile("White",R.drawable.white_piano_tile)
+    val blackTile = PianoTile("Black",R.drawable.black_piano_tile)
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val currentScore = 0;
+        var currentScore = 0
         super.onCreate(savedInstanceState)
         setContentView(R.layout.grid)
-
-        val whiteTile = PianoTile("White",R.drawable.white_piano_tile)
-        val blackTile = PianoTile("Black",R.drawable.black_piano_tile)
-        val tileRow = ArrayList<PianoTile>()
-        tileRow.add(blackTile)
-        tileRow.add(whiteTile)
-        tileRow.add(whiteTile)
-        tileRow.add(whiteTile)
-        val tiles = ArrayList<PianoTile>()
-        for (i in 1..5){
-            Collections.shuffle(tileRow)
-            tiles.addAll(tileRow)
-
-        }
+        gameGrid.addAll(generateRow())
+        gameGrid.addAll(generateRow())
+        gameGrid.addAll(generateRow())
 
 
 
 
 
-
-
-
-
-        val adapter = GridViewAdapter(this,tiles)
-
-        grid.adapter = adapter
+        var adapter = GridViewAdapter(this,gameGrid)
         startGame(currentScore)
+        grid.adapter = adapter
 
     }
 
     fun startGame(currentScore : Int){
         grid.setOnItemClickListener{parent, view, position, id ->
+            val tile:PianoTile = grid.adapter.getItem(position) as PianoTile
+            println(tile)
+            if(tile.color == "Black")
+            {
+                val column = position%4
+                val row = position/4
+                Toast.makeText(this,"row: $row, column: $column",Toast.LENGTH_SHORT).show()
+                deleteRow()
+            } else
+            {
+                Toast.makeText(this,"Game Over",Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 
-    fun generateRow(){
+    fun generateRow(): ArrayList<PianoTile> {
+        var tileRow = ArrayList<PianoTile>()
+        tileRow.add(PianoTile("White",R.drawable.white_piano_tile))
+        tileRow.add(PianoTile("White",R.drawable.white_piano_tile))
+        tileRow.add(PianoTile("White",R.drawable.white_piano_tile))
+        tileRow.add(PianoTile("Black",R.drawable.black_piano_tile))
+        Collections.shuffle(tileRow)
+        return tileRow
+    }
+
+    fun deleteRow()
+    {
+        for (i in 0..3)
+        {
+            gameGrid.removeAt(i)
+        }
+        gameGrid.addAll(generateRow())
+        var adapter = GridViewAdapter(this,gameGrid)
+        grid.adapter = adapter
     }
 }
--- app/src/main/java/com/example/pianotiles/GridViewAdapter.kt
+++ app/src/main/java/com/example/pianotiles/GridViewAdapter.kt
@@ -2,23 +2,25 @@ package com.example.pianotiles
 
 import android.content.Context
 import android.support.v7.widget.AppCompatTextView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import kotlinx.android.synthetic.main.item_layout.view.*
 
-class GridViewAdapter(val context: Context, val rowofButtons: List<PianoTile>) : BaseAdapter(){
+class GridViewAdapter(val context: Context, val rowofButtons: ArrayList<PianoTile>) : BaseAdapter(){
+    var gridCopy = rowofButtons
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val tile = this.rowofButtons[position]
+        val tile = gridCopy[position]
         var view: View = LayoutInflater.from(context).inflate(R.layout.item_layout,parent,false)
         view.imgTile.setImageResource(tile.image!!)
-
-        val gridItem = view.findViewById<AppCompatTextView>(R.id.item_text_view)
-
-
         return view
+
     }
+
+
 
     override fun getItem(position: Int): PianoTile {
         return rowofButtons[position]
