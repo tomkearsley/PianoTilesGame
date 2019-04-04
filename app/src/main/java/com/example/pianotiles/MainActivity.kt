d0592bee4b2be89b996aedcb909b8dcb9d4f5c07
Tom Kearsley <tke29@uclive.ac.nz>
2019-04-04 16:53:14 +1300

    Added animation to transition label to Green.

--- app/src/main/java/com/example/pianotiles/MainActivity.kt
+++ app/src/main/java/com/example/pianotiles/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.pianotiles
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -13,6 +17,7 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var titleText = findViewById<TextView>(R.id.titleText)
         val playButton: Button = findViewById(R.id.playButton)
         playButton.setOnClickListener {
             val intent = Intent(this, GridActivity::class.java)
@@ -23,6 +28,13 @@ class MainActivity : AppCompatActivity(){
             val intent = Intent(this, Leaderboard::class.java)
             startActivity(intent)
         }
+        var colors = arrayOf(ColorDrawable(Color.WHITE), ColorDrawable(Color.GREEN))
+        var trans = TransitionDrawable(colors)
+        titleText.setBackground(trans)
+        trans.startTransition(6000)
+
+
+
     }
 
 
